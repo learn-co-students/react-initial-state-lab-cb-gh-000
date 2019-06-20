2e453c10242c4bb1c90cdb551fab2b3ace0c8124
Jordan de Souza <ja3desou@uwaterloo.ca>
2019-06-20 14:33:14 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,17 @@
-// your Bomb code here!
+import React from "react";
+
+class Bomb extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state{
+      secondsLeft: this.initialCount;
+    }
+  }
+  render(){
+    return(
+      <div className="Bomb">
+        (this.state.secondsLeft ===0) ? Boom! : this.state.secondsLeft seconds left before I go boom!
+    )
+  }
+}
